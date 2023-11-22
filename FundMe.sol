5f3059b2cc06e2b04be4ea949f45ebe36ff684c2
CodeQueenTosin <CodeQueenTosin@gmail.com>
2023-11-22 13:26:11 +0100

    Added validation for sending Money

--- FundMe.sol
+++ FundMe.sol
@@ -4,5 +4,19 @@ pragma solidity   0.8.18;
 
 contract FundMe {
 
+function  Fund()  public payable {
+   //Allow users  send money
+   // Have a minimum value to be sent in USD
+    //use paaybale lkeu
+    //msg.value
+    require(msg.value >  1e18, "didint send enought ether")    ;
+
+} 
+
+
+
+//function Withdraw  () public {
+
+//s}
 
 }
